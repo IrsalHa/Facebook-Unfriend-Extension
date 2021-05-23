63ee4a2bc4efeba2a0304d3fd97c8f4c0ac88385
irsalSchwartz <irsalhakim10@gmail.com>
2021-05-23 12:27:05 +0700

    reworked update & script

--- sc.js
+++ sc.js
@@ -4,9 +4,28 @@ let btnupdate = document.getElementById("hahaupdate");
 btnupdate.addEventListener("click", (event) => {
   document.getElementById('aab').style.color = "blue";
   document.getElementById('aab').innerHTML = "Loading!";
-  fetch("https://pastebin.com/raw/RRwsFKmw").then(response => response.json())
+  fetch("https://pastebin.com/raw/RRwsFKmw",{
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "max-age=0",
+      "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1"
+    },
+    "referrer": "https://pastebin.com/RRwsFKmw",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  }).then(response => response.json())
 .then(data => {
-  let my_v = 0;
+  let my_v = 1;
   if(my_v < data.v){
     document.getElementById('aab').style.color = "red";
     document.getElementById('aab').innerHTML = "Please Update First! <a target='_blank' href='https://github.com/IrsalHa/Facebook-Unfriend-Extension/releases/'> Click Here</a> ";
